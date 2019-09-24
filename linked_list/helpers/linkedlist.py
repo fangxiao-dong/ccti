4a3b43bb98efd88307522bc350c863507ce5a5ae
Fangxiao Dong <fangxiao.dong@outlook.com>
2019-09-23 22:04:34 -0400

    add example linked list data structure

---
+++ linked_list/helpers/linkedlist.py
@@ -0,0 +1,36 @@
+class Node(object):
+    """A class for Linked List Data Structure"""
+
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+        def append(self, data):
+            node = Node(data)
+            current = self
+
+            while current.next is not None:
+                current = current.next
+
+            current.next = node
+
+        def delete(self, head, data):
+            current = head
+
+            if current.data == data:
+                return current.next
+
+            while current.next is not None:
+
+                if current.next.data == data:
+                    current.next = current.next.next
+                    return head
+
+                current = current.next
+
+            return head
+
+
+
+
+        
